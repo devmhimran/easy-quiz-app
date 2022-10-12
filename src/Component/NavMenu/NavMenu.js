c3139afcde7233b8558dd0422692adf12c910279
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-10-12 06:40:06 +0600

    Update Navbar Logo Link

--- src/Component/NavMenu/NavMenu.js
+++ src/Component/NavMenu/NavMenu.js
@@ -7,7 +7,7 @@ const NavMenu = () => {
         <div>
             <Navbar bg="light" expand="lg">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="/">Easy Quiz App</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
