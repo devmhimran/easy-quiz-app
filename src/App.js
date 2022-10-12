5573cc297b8582c5ab428c2f23b355afdf83e6a9
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-10-12 20:16:43 +0600

    Added Error Page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavMenu from './Component/NavMenu/NavMenu';
 import Statistics from './Component/Statistics/Statistics';
 import QuizMain from './Component/QuizMain/QuizMain';
 import Blog from './Component/Blog/Blog';
+import ErrorPage from './Component/ErrorPage/ErrorPage';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route path="/statistics" element={<Statistics />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/quiz/:id" element={<QuizMain/>} />
+          <Route path="*" element={<ErrorPage/>} />
+
         </Routes>
     </div>
   );
